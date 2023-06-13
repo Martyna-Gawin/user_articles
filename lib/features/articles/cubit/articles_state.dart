62d70d1f4e8a53a9644db6645c593732b6217c74
Martyna Gawin <martynagawin1@gmail.com>
2023-06-13 11:46:53 +0200

    save all changes

--- lib/features/articles/cubit/articles_state.dart
+++ lib/features/articles/cubit/articles_state.dart
@@ -1,5 +1,14 @@
 part of 'articles_cubit.dart';
 
+// @freezed
+// class ArticlesState with _$ArticlesState {
+//   factory ArticlesState(
+//     @JsonKey(name: 'results = const[]') List<ArticleModel> results,
+//     @JsonKey(name: 'status = status.initial') Status status,
+//     @JsonKey(name: 'errorMessage') String? errorMessage,
+//   ) = _ArticlesState;
+// }
+
 class ArticlesState {
   ArticlesState({
     this.results = const [],
